89a1c54f019c260d4c87798e8f08df84016218c7
tbgamal <tbgamalm@gmail.com>
2023-11-17 09:10:33 -0600

    minor changes to Navigations.jsx linking Logout to home

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -9,7 +9,7 @@ function Navigations ({ token, setToken }) {
       <ul className='nav-menu'>
         <li><Link to="/">Home</Link></li>
         {token ? <li><Link to="/account">MyAccount</Link></li> : <div></div> }
-        {token ? <li onClick={()=>setToken(null)}><Link>Logout</Link></li> : <div></div>}
+        {token ? <li onClick={()=>setToken(null)}><Link to="/">Logout</Link></li> : <div></div>}
         {token ? <div></div> : <li><Link to="/login">Login</Link></li>}
         {token ? <div></div> : <li><Link to="/register">Sign up</Link></li>}
       </ul>
